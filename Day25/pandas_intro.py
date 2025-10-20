e409ad59c2e5b1ef2e3c20db8c905f2896e0c6c7
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 10:37:06 +0200

    change the data type commands

--- Day25/pandas_intro.py
+++ Day25/pandas_intro.py
@@ -1,5 +1,11 @@
 import pandas
 
 data= pandas.read_csv("weather_data.csv")
-print(type(data))
-print(type(data["temp"]))
+# print(type(data))
+temp_data_list= data["temp"].to_list()
+
+print(temp_data_list)
+print(type(temp_data_list))
+
+# data_dict= data.to_dict()
+# print(data_dict)
