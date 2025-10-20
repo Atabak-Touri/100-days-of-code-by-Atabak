d188432dc3f3a4108cacaca01634d008cedd3333
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 10:41:21 +0200

    summary statistics test using pandas

--- Day25/pandas_intro.py
+++ Day25/pandas_intro.py
@@ -3,9 +3,13 @@ import pandas
 data= pandas.read_csv("weather_data.csv")
 # print(type(data))
 temp_data_list= data["temp"].to_list()
+avg_temp= sum(temp_data_list)/ len(temp_data_list)
+"""summary statistics"""
 
-print(temp_data_list)
-print(type(temp_data_list))
+print(data["temp"].max())
+print(data["temp"].mean())
+print(avg_temp)
+# print(type(temp_data_list))
 
 # data_dict= data.to_dict()
 # print(data_dict)
