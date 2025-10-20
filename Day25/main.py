085a58e8f1c823ef929c89a1e2454145f5c04b53
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 11:27:59 +0200

    using pands to extract a specific kind of data out of our csv file and then make a dictionary out of it. in the end converting that dict to a new csv file with desired values

--- Day25/main.py
+++ Day25/main.py
@@ -1,3 +1,31 @@
+import pandas
+
+data= pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+Gray_sqrl= data[data["Primary Fur Color"] == "Gray"]
+Cinnamon_sqrl= data[data["Primary Fur Color"] == "Cinnamon"]
+Black_sqrl= data[data["Primary Fur Color"] == "Black"]
+
+print(len(Gray_sqrl))
+print(len(Cinnamon_sqrl))
+print(len(Black_sqrl))
+
+data_dict= {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [Gray_sqrl,Cinnamon_sqrl,Black_sqrl]
+}
+df=pandas.DataFrame(data_dict).to_csv("Squirrel Count")
+
+
+
+
+
+
+
+
+
+
+
+
 # # with open("weather_data.csv") as weather_data:
 # #     data = weather_data.readlines()
 # #     print(data)
@@ -11,12 +39,12 @@
 #             temperature.append(int(row[1]))
 #     print(temperature)
 # How to create a dataframe from scratch
-import pandas
-
-data_dict = {
-    "students": ["Andy", "Josi", "Maria"],
-    "scores" : [16,56, 65]
-}
-data= pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-print(data)
+# import pandas
+#
+# data_dict = {
+#     "students": ["Andy", "Josi", "Maria"],
+#     "scores" : [16,56, 65]
+# }
+# data= pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+# print(data)
