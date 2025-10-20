2cf37c6ed1d8259fd46baaa27d0ea675db07ee3c
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 12:20:41 +0200

    now I have another capability to figure out which states I forgot or I did not guess. then these states are going to be printed in my consul.

--- Day25/us-states-game-start/main.py
+++ Day25/us-states-game-start/main.py
@@ -16,6 +16,13 @@ while len(guessed_states)< 50:
                                     prompt="what's the state's name?").title() #title() will make the first letter in capital
 
     # if the answer_state is one of the states in our csv file
+    if answer_state == "Exit":
+        missing_state= []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        print(missing_state)
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -25,6 +32,7 @@ while len(guessed_states)< 50:
         t.goto(state_data.x.item(), state_data.y.item())
     # if it is, make the turtle to write the name of the state on the location
         t.write(answer_state)
+#states to learn
 
 # def get_mouse_click_coor(x, y): #this will show my mouse coordination on screen
 #     print(x, y)
