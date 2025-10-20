f3980275e331b7b1a68f35778c3052dd2d5845d5
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 10:22:45 +0200

    introduction to pandas and see how the commands are getting shorter compared to previous file

--- Day25/main.py
+++ Day25/main.py
@@ -1,12 +1,13 @@
+# # with open("weather_data.csv") as weather_data:
+# #     data = weather_data.readlines()
+# #     print(data)
+#
+# import csv
 # with open("weather_data.csv") as weather_data:
-#     data = weather_data.readlines()
-#     print(data)
+#     data= csv.reader(weather_data)
+#     temperature= []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
 
-import csv
-with open("weather_data.csv") as weather_data:
-    data= csv.reader(weather_data)
-    temperature= []
-    for row in data:
-        if row[1] != "temp":
-            temperature.append(int(row[1]))
-    print(temperature)
