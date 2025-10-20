052aa0088872259fca51c107794f21ee5a9e02f5
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 10:46:02 +0200

    searching a term in our data frame

--- Day25/pandas_intro.py
+++ Day25/pandas_intro.py
@@ -2,14 +2,12 @@ import pandas
 
 data= pandas.read_csv("weather_data.csv")
 # print(type(data))
-temp_data_list= data["temp"].to_list()
-avg_temp= sum(temp_data_list)/ len(temp_data_list)
-"""summary statistics"""
-
-print(data["temp"].max())
-print(data["temp"].mean())
-print(avg_temp)
+# temp_data_list= data["temp"].to_list()
+# avg_temp= sum(temp_data_list)/ len(temp_data_list)
+# print(data["Condition"])
+# print(data.Condition)
 # print(type(temp_data_list))
 
 # data_dict= data.to_dict()
 # print(data_dict)
+print(data[data.day == "Monday"])
