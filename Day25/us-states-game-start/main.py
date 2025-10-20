3985e511c721d9c683ad63ad74b04c35240a76a8
Atabak-Touri <atabak.touri@stud.th-deg.de>
2025-10-20 12:09:29 +0200

    defining the name of states and getting input for comparison

--- Day25/us-states-game-start/main.py
+++ Day25/us-states-game-start/main.py
@@ -1,20 +1,37 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
+data= pandas.read_csv("50_states.csv")
+all_states= data.state.to_list()
 
+guessed_states= []
 
+while len(guessed_states)< 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct",
+                                    prompt="what's the state's name?").title() #title() will make the first letter in capital
 
-def get_mouse_click_coor(x, y): #this will show my mouse coordination on screen
-    print(x, y)
+    # if the answer_state is one of the states in our csv file
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data= data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+    # if it is, make the turtle to write the name of the state on the location
+        t.write(answer_state)
 
-turtle.onscreenclick(get_mouse_click_coor) #event listen
-
-turtle.mainloop() #alternative way to keep the screen open
+# def get_mouse_click_coor(x, y): #this will show my mouse coordination on screen
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor) #event listen
+#
+# turtle.mainloop() #alternative way to keep the screen open
 
 
 screen.exitonclick()
